26fed70fae8c8932914f43cf1f7caef7fbf5c100
Mateo Silguero <mfsilguero@gmail.com>
2020-04-28 20:31:31 -0300

    final form

--- pokedex2/App.js
+++ pokedex2/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, StatusBar} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import Home from './src/views/Home';
@@ -8,10 +8,36 @@ import Detail from './src/views/Detail';
 
 const Stack = createStackNavigator();
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#E3350D',
+    normal: '#6D6D4E',
+    fire: '#F08030',
+    fighting: '#C03028',
+    water: '#6890F0',
+    flying: '#A890F0',
+    grass: '#78C850',
+    poison: '#A040A0',
+    electric: '#F8D030',
+    ground: '#E0C068',
+    psychic: '#F85888',
+    rock: '#B8A038',
+    ice: '#98D8D8',
+    bug: '#A8B820',
+    dragon: '#7038F8',
+    ghost: '#705898',
+    dark: '#705848',
+    steel: '#B8B8D0',
+    fairy: '#EE99AC',
+  },
+};
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" backgroundColor="#ff0000" />
+    <NavigationContainer theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="#f4511e" />
       <Stack.Navigator
         screenOptions={{
           headerStyle: {
--- pokedex2/src/views/Detail.js
+++ pokedex2/src/views/Detail.js
@@ -1,29 +1,143 @@
 import React, {useEffect, useState} from 'react';
-import {Image, Text, View} from 'react-native';
-import P from '../services/pokeapi';
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {useTheme} from '@react-navigation/native';
+import P, {Pokemon} from '../services/pokeapi';
+import {ScrollView} from 'react-native-gesture-handler';
 
-const Detail = ({route}) => {
+const Detail = ({route, navigation}) => {
   const [poke, setPoke] = useState({});
-  const {pokemon} = route.params;
+  const {colors} = useTheme();
+  const {pokemon, previous} = route.params;
+
+  const colorByType = colors[poke.types?.[0].type.name];
+
+  const recursiveEvolutionChain = (chain) => {
+    const isCurrent = chain?.species.name === pokemon.name;
+    return (
+      <View style={styles.row}>
+        <TouchableOpacity
+          onPress={() => {
+            if (previous === chain?.species.name) {
+              navigation.goBack();
+            } else if (!isCurrent) {
+              navigation.push('Pokemon', {
+                pokemon: chain.species,
+                previous: pokemon.name,
+              });
+            }
+          }}>
+          <Text
+            style={[
+              styles.pokemonName,
+              {
+                marginHorizontal: 16,
+                color: isCurrent && colors.primary,
+              },
+            ]}>
+            {chain?.species.name}
+          </Text>
+        </TouchableOpacity>
+        {chain?.evolves_to.length > 0 && (
+          <Text style={styles.pokemonName}>></Text>
+        )}
+        <View>
+          {chain?.evolves_to?.map((poke) => recursiveEvolutionChain(poke))}
+        </View>
+      </View>
+    );
+  };
 
   useEffect(() => {
-    P.getPokemonByName(pokemon.name).then(function (response) {
-      setPoke({...pokemon, ...response});
+    Promise.all([
+      P.getPokemonByName(pokemon.name),
+      P.getPokemonSpeciesByName(pokemon.name),
+    ]).then(([pokemonInfo, speciesInfo]) => {
+      const {types, id} = pokemonInfo;
+      const {evolution_chain, flavor_text_entries} = speciesInfo;
+      const description = flavor_text_entries
+        .find((flavor) => flavor.language.name === 'es')
+        ?.flavor_text.replace(/\n/g, ' ');
+      P.resource(evolution_chain.url).then((evolutionChain) => {
+        setPoke(
+          Pokemon({
+            ...pokemon,
+            id,
+            description,
+            chain: evolutionChain.chain,
+            types: types.sort((a, b) =>
+              a.slot > b.slot ? 1 : a.slot < b.slot ? -1 : 0,
+            ),
+          }),
+        );
+      });
     });
   }, []);
 
+  useEffect(() => {
+    navigation.setOptions({
+      headerStyle: {
+        backgroundColor: colorByType,
+      },
+    });
+  }, [colorByType]);
+
   return (
-    <View>
-      <Image style={{width: 50, height: 50}} source={{uri: pokemon.image}} />
-      <Text>
-        #{pokemon.id} - {pokemon.name}
-      </Text>
-      {}
-      <Text>
-        #{pokemon.id} - {pokemon.name}
-      </Text>
+    <View style={{padding: 16}}>
+      <StatusBar backgroundColor={colorByType} barStyle="light-content" />
+      <View style={styles.row}>
+        <Image style={{width: 180, height: 180}} source={{uri: poke.image}} />
+        <View style={{paddingTop: 24}}>
+          <Text style={styles.pokemonName}>
+            #{poke.id} - {poke.name}
+          </Text>
+          <View style={styles.typesContainer}>
+            {poke.types?.map((type, index) => (
+              <Text
+                key={index}
+                style={[
+                  styles.type,
+                  {backgroundColor: colors[type.type.name]},
+                ]}>
+                {type.type.name}
+              </Text>
+            ))}
+          </View>
+        </View>
+      </View>
+      <Text style={{fontSize: 16}}>{poke.description}</Text>
+      <Text style={{fontSize: 24, marginTop: 16}}>Cadena Evolutiva</Text>
+      <ScrollView horizontal style={styles.typesContainer}>
+        {recursiveEvolutionChain(poke.chain)}
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+  },
+  pokemonName: {
+    fontSize: 20,
+  },
+  typesContainer: {
+    flexDirection: 'row',
+    marginTop: 8,
+  },
+  type: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 16,
+    marginRight: 8,
+    color: 'white',
+  },
+});
 
 export default Detail;
